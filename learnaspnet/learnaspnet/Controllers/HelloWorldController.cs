6e5c9505322405b0bf3a1b01aeaebc0d4f2502f6
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-08-10 15:17:49 -0400

    controllers

---
+++ learnaspnet/learnaspnet/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        //GET: /HelloWorld/
+        //HelloWorldController is URL /HelloWorld/, anything() = /anything/
+        //Under startup.cs, Index is set as the default action, meaning /Index/ or / loads Index()
+        //NOTE: does not auto-update like React; must re-run to add changes to page (option+command+return)
+        //website.com/[Controller]/[ActionName]/[Parameters]
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        //
+        //GET: /HelloWorld/Welcome/
+        //Welcome() = /Welcome/
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
