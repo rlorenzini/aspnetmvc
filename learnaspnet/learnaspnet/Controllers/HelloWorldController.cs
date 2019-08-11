111231d8752fc1b399f93641b8a8a1bdc4827274
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-08-10 21:31:44 -0400

    parameters

--- learnaspnet/learnaspnet/Controllers/HelloWorldController.cs
+++ learnaspnet/learnaspnet/Controllers/HelloWorldController.cs
@@ -6,24 +6,28 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     {
         //
-        //GET: /HelloWorld/
-        //HelloWorldController is URL /HelloWorld/, anything() = /anything/
-        //Under startup.cs, Index is set as the default action, meaning /Index/ or / loads Index()
-        //NOTE: does not auto-update like React; must re-run to add changes to page (option+command+return)
-        //website.com/[Controller]/[ActionName]/[Parameters]
+        // GET: /HelloWorld/
+        // HelloWorldController is URL /HelloWorld/, anything() = /anything/
+        // Under startup.cs, Index is set as the default action, meaning /Index/ or / loads Index()
+        // NOTE: does not auto-update like React; must re-run to add changes to page (option+command+return)
+        // website.com/[Controller]/[ActionName]/[Parameters]
 
         public string Index()
         {
-            return "This is my default action...";
+            return "This is my default action..."; // MUST end in ; 
         }
 
         //
-        //GET: /HelloWorld/Welcome/
-        //Welcome() = /Welcome/
+        // GET: /HelloWorld/Welcome/
+        // Welcome() = /Welcome/
+        // Requires using System.Text.Encodings.Web;
+        // numTimes is set to default 1 if no input specified; string name needs an input
+        // /HelloWorld/Welcome?name=Rick&numtimes=4
+        // ? defines start of parameters, & connects multiple parameters
 
-        public string Welcome()
+        public string Welcome(string name, int numTimes = 1)
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}.");
         }
     }
 }
