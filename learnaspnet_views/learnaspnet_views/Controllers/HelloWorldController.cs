770572b877f51b926c052edcf542ebb943be50e4
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-08-10 21:39:32 -0400

    starting views

---
+++ learnaspnet_views/learnaspnet_views/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        public string Index()
+        {
+            return "This is my default action..."; // MUST end in ; 
+        }
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}.");
+        }
+    }
+}
+
